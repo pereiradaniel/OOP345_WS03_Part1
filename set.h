523c184ea097b8e0100d8af6bd61bc50e7e1f645
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-05 23:20:48 -0500

    set.h built, does not compile yet without pair.h but no errors shown yet in set.h

--- set.h
+++ set.h
@@ -2,7 +2,30 @@
 #define SDDS_SET_H
 
 namespace sdds {
+	template <class T, unsigned int N>
+	class Set {
+		T result[N];			// The type of any element in the collection.
+		unsigned elements = 0;	// Initially the collection has no elements.
+	public:
+		// Return the current number of elements in the collection.
+		size_t size() const {
+			return elements;
+		}
 
+		// Return a reference to the element at index 'idx' of the statically allocated array.
+		// Assume that the parameter is valid.
+		const T& operator[] (size_t idx) const {
+			return result[idx] ;
+		}
+
+		// If the collection has capacity for another element, add a copy of 'item' to the collection.
+		// Otherwise, does nothing.
+		void operator+=(const T& item) {
+			if (elements < N) {
+				result[elements] = item;	// Add item to results.
+				elements++;					// Increment number of elements.
+			}
+		}
+	};
 }
-
 #endif
